d1359b09c85f72f194e4c9ed5a4d9e8f2a130857
Sophie Prietto <sophprietto@live.com>
2019-04-11 04:18:08 +0000

    Starting to try n use strings .

--- main.c
+++ main.c
@@ -1,7 +1,17 @@
+void caesarCipher(char* a, int b);
+
 #include <stdio.h>
 
-main() {
+int main(void) {
 
-  return 0;
+int b = 1;
+char letter[101];
+int text;
+
+//ask user for text to encrypt
+printf("Enter the text you wish to encrypt: ");
+
+scanf("%c ")
+ 
 
 }
