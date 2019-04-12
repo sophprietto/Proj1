c9050419f7f38b2d87ecfa7124d7e20a006625bb
Sophie Prietto <sophprietto@live.com>
2019-04-12 01:18:51 +0000

    j

--- main.c
+++ main.c
@@ -3,16 +3,23 @@
 int main()
 {
 	char cipher[101], a;
-	int i, key;
+	int i, key = 1;
 	
 	printf("Please enter a message to encrypt: ");
 	
-	// figure out how to get it?
+	for(i = 0; cipher[i] != '\0'; ++i){
+	    
+        if ((cipher[i] > 96) && (cipher[i] < 123)){
+    
+            cipher[i] = cipher[i]-32;
+}
+
+	if(str[i] > 65) {
+	    
+	    cipher[i] = cipher[i] + key;
+	       
+	   }
 	
-	printf("Enter key: ");
 	
-	scanf("%d", &key);
 	
-	for(i = 1; cipher[i] != '\0'; ++i){ // starts at a ,null, increments +1
-	a = cipher[i];
 	
