084fa2ae100a0f114a5590a32dfa45b9191b69d7
Sophie Prietto <sophprietto@live.com>
2019-04-20 06:24:53 +0000

    sub encrypt and decrypt - wont move backwards to z and wont convert uppercase letters

--- main.c
+++ main.c
@@ -1,35 +1,81 @@
+void subencrypt (char ci[]);
+void subdecrypt (char ci[]);
+
 #include<stdio.h>
- 
-void main()
+
+void main ()
 {
-	char ci[500], a;
-	int i, key = ;
-	
-	
-	printf("Please enter a message to encrypt: ");
-	scanf("%s", &ci);
-	
-	printf("Please Enter a key: ");
-	scanf("%d", &key);
-	
-	for(i = 0; ci[i] != '\0'; i++){
-	    
-        if ((ci[i] > 96) && (ci[i] < 123)){ // 
-    
-            ci[i] = ci[i]-32;
+  char ci[500];
+  int i, funct;
+
+
+  printf ("Please enter a message to encrypt: ");
+  scanf ("%s", &ci);
+
+  printf ("Choose function: ");
+  scanf ("%d", &funct);
+
+  switch (funct) {
+    case 0:
+      subencrypt (ci);
+      break;
+    case 1:
+      subdecrypt (ci);
+      break;
     }
+}
 
+void
+subencrypt (char ci[])
+{
 
-	if((ci[i] > 65 && ci[i] < 91)) {
+  int i, key;
+
+  printf ("Enter key: ");
+  scanf ("%d", &key);
+
+  for (i = 0; ci[i] != '\0'; i++) {
+
+      if ((ci[i] > 96) && (ci[i] < 123)) {			
+
+	  ci[i] = ci[i] - 32 + key;
+
+	 
+	    if ((ci[i]) > 90) {
+	        
+	        ci[i] = ci[i] - 26;
+	    }
+      }
+	     else if ((ci[i]))
 	    
-	  
-	    ci[i] = ci[i] + (key % 26);
-	       
-	   }
-   }
 
-	
+	}
 
-	printf("%s", ci);
-	
+  printf ("%s", ci);
+
+}
+
+void subdecrypt (char ci[]) {
+
+  int i, key;
+
+  printf ("Enter key: ");
+  scanf ("%d", &key);
+
+  for (i = 0; ci[i] != '\0'; i++) {
+
+      if ((ci[i] > 96) && (ci[i] < 123)) {		
+
+	  ci[i] = ci[i] - 32 - key;	
+
+	  if ((ci[i] > 65 && ci[i] < 91)) {	
+            ci[i] = ci[i] - key;
+
+	  if ((ci[i]) < 65) {
+            ci[i] = ci[i] + 26;
+	    }
+      }
+    }
+}
+  printf ("%s", ci);
 }
