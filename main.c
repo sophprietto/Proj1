3130bc65b22b609595ddbeba8d211ce8ad9c9fdf
Sophie Prietto <sophprietto@live.com>
2019-04-28 11:59:35 +0000

    Started substitution cipher encryption - bare bones

--- main.c
+++ main.c
@@ -1,6 +1,3 @@
-/*
-*/
-
 void rotencrypt (char ci[]); //this funcion will be called to encrypt a rotation cipher
 void rotdecrypt (char ci[]); //this function will be called to decrypt a rotation cipher
 void subencrypt (char ci[]); //this function when called will encrypt a substitution cipher
@@ -13,14 +10,13 @@ int main (void) {
   int funct; // this will call certain functions within the switch case
   
   
-  
+  printf("Choose 0 for a rotation encryption, and 1 for decryption.\nChoose 2 for a substitution encryption, and 3 for a decryption.\n");
   printf("Please choose a function to execute: "); // prompts choice of function to use after input of text
   scanf("%d\n", &funct); //scans message
 
   printf("\nPlease enter message: "); // prompts entry of the message 
   scanf("%[^\n]s", ci); // scans the message
 
-  
   
 /* the switch case calls the function which will be used to encrypt of decrypt the message inputted by the user */
 
@@ -29,15 +25,14 @@ int main (void) {
       rotencrypt (ci); // calls the function which will encrypt a rotation cipher
       break; // code exits after execution of function 0
     case 1: // when 1 is inputted it will call this function
-      rotdecrypt (ci); // calls the function whihc decrypts
+      rotdecrypt (ci); // calls the function which decrypts
       break; // code exits after exectution of function 1
-    case 2:
+    case 2: //calls the function which will encrypt a substitution cipher
       subencrypt (ci);
-      break;
-    case 3:
+      break;// code exits after execution of function
+    case 3: //calls the function which will decrypt a substitution cipher
       subdecrypt (ci);
-      break;
-        
+      break; // code exits after execution of function
     }
 }
 
@@ -100,6 +95,26 @@ void rotdecrypt (char ci[]) {
   
   void subencrypt (char ci[]) {
       
+      char ci[100] = {"abcdefg hijklmn OPQRSTUV wxyz"};
+      char cipheralphabet[26] = {"QWERTYUIOPLKJHGFDSAZXCVBNM"};
       
+      int i, n;
+      
+      for (i = 0; ci[i] != '\0'; i++) {
+          
+          if (ci[i] >= 97 && ci[i] <= 122) {
+              ci[i] = ci[i] -32;
+          }
+          if (ci[i] >= 65 && ci[i] <= 90) {
+              ci[i] = ci[i] - 65;
+              n = ci[i];
+              ci[i] = cipheralphabet[n];
+          }
+      scanf("%s", ci);
+      printf("%s", ci);
+      
+  }
+  
+  void subdecrypt (char ci[]) {
       
   }
